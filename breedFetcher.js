c498ed9bbe474d1823655b2594db2e45483c411e
Eina Onting <einers@outlook.com>
2020-01-16 22:28:29 +0000

    lint breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,18 @@
 const request = require('request');
 
-const fetchBreedDescription = (breedName, callback) => {  
+const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
-    if(err) {
-      callback(err, null);      
-    }    
+    if (err) {
+      callback(err, null);
+    }
     const data = JSON.parse(body);
     if (!data && !data[0]) {
       callback(null, `${breedName} was not found. Please make sure that ${breedName} is a cat.`);
     }
-    if(data && data[0]) {
+    if (data && data[0]) {
       callback(null, data[0].description);
-    }     
-  });  
+    }
+  });
 };
 
 module.exports = { fetchBreedDescription };
