024029a5fd93d2813ae88a3173c9723e613b8ada
Eina Onting <einers@outlook.com>
2020-01-16 22:27:59 +0000

    refactor & eslint fetchBreedDescription

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,18 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-
-request(`https://api.thecatapasdasdi.com/v1/breeds/search?q=${args[0]}`, (err, res, body) => {
-  if (res && res.statusCode === 200) {
+const fetchBreedDescription = (breedName, callback) => {  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
+    if(err) {
+      callback(err, null);      
+    }    
     const data = JSON.parse(body);
-    if (data && data[0]) {
-      console.log(data[0].description);
-    } else {
-      console.log(`${args[0]} was not found. Please make sure that ${args[0]} is a cat.`);
+    if (!data && !data[0]) {
+      callback(null, `${breedName} was not found. Please make sure that ${breedName} is a cat.`);
     }
-  } else {
-    console.log(err);
-  }
-  
-});
+    if(data && data[0]) {
+      callback(null, data[0].description);
+    }     
+  });  
+};
+
+module.exports = { fetchBreedDescription };
